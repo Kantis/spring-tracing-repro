2256b1c1f0e5f94964b9d3868ab939b21dba7373
Emil Kantis <emil.kantis@protonmail.com>
2025-04-29 11:38:29 +0200

    kotest spring extension might be the culprit..?

--- src/test/kotlin/dev/databoss/AutoconfiguredPropagatorTest.kt
+++ src/test/kotlin/dev/databoss/AutoconfiguredPropagatorTest.kt
@@ -5,14 +5,19 @@ import io.kotest.inspectors.forOne
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.opentelemetry.context.propagation.ContextPropagators
+import io.opentelemetry.context.propagation.TextMapPropagator
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 
-@SpringBootTest
+@SpringBootTest(properties = ["management.tracing.enabled=true"])
 class AutoconfiguredPropagatorTest(
    val applicationContext: ApplicationContext,
 ) : FunSpec(
    {
+      test("Application context should have management.tracing.enabled = true"){
+         applicationContext.environment.getProperty("management.tracing.enabled") shouldBe "true"
+      }
+
       test("The no-op propagator should not be used") {
          val propagators = applicationContext.getBeansOfType(ContextPropagators::class.java)
          propagators.forOne {
@@ -29,6 +34,11 @@ class AutoconfiguredPropagatorTest(
             it.value.textMapPropagator::class.java.name shouldBe
                "org.springframework.boot.actuate.autoconfigure.tracing.CompositeTextMapPropagator"
          }
+      }
+
+      test("B3 multi-propagator should be used") {
+         val textMapPropagators = applicationContext.getBeansOfType(TextMapPropagator::class.java)
+         println(textMapPropagators)
       }
    },
 )
